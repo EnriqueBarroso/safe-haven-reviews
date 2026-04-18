f2dadb0a486c16bfd9bf839d8b21374bc340372f
Enrique <enrique.designer2022@gmail.com>
2026-04-18 16:00:44 +0200

    fix: engañar a supabase en el build

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,5 +3,8 @@ import { createBrowserClient } from '@supabase/ssr'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
+process.env.NEXT_PUBLIC_SUPABASE_URL || "https://tu-proyecto-falso.supabase.co"
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "llave-falsa-para-engañar-a-vercel"
+
 // Usamos createBrowserClient en lugar de createClient
 export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey)
